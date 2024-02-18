af081d747ab924cbc041d48fc876460dc51118b4
Mendium <160427960+Mendium@users.noreply.github.com>
2024-02-19 02:00:10 +0300

    Update FAQHandler.go

--- calculator_orchestrator/handlers/FAQHandler.go
+++ calculator_orchestrator/handlers/FAQHandler.go
@@ -15,5 +15,5 @@ func OperataionsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getOperationsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Operations:\n\"*\" requires 10000ms,\n\"+\" requires 2000ms (replaced with 'p'),\n\"-\" requires 2500ms,\n\"/\" requires 12500ms.")
+	fmt.Fprintf(w, "Operations:\n\"*\" requires 5ms,\n\"+\" requires 7ms (replaced with 'p'),\n\"-\" requires 3ms,\n\"/\" requires 8ms.")
 }
